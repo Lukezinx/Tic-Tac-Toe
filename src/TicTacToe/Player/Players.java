02a5c201d464cd6301bb5c501a1ff39f614b9987
Lucas <Lucasdeapool@gamil.com>
2025-04-30 20:13:15 -0300

    add method and the logic to play, calling Board

--- src/TicTacToe/Player/Players.java
+++ src/TicTacToe/Player/Players.java
@@ -1,5 +1,41 @@
 package TicTacToe.Player;
 
+import TicTacToe.Board.Board;
+import TicTacToe.Symbols.Symbols;
+import java.util.*;
+
 public class Players {
 
+    private String nome;
+    private Symbols symbol;
+
+    public Players(String nome, Symbols symbols) {
+        this.nome = nome;
+        this.symbol = symbols;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public Symbols getSymbol() {
+        return symbol;
+    }
+
+    public void play(Board board) {
+        Scanner sc = new Scanner(System.in);
+        int row;
+        int col;
+        do {
+            System.out.println("tell me the column and row of your move");
+            System.out.print("Row -> ");
+            row = sc.nextInt();
+            sc.nextLine();
+            System.out.print("Collum -> ");
+            col = sc.nextInt();
+            sc.nextLine();
+
+        } while (!board.placeSymbol(row, col,this.symbol));
+    }
 }
