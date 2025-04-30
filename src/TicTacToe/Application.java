df06ac7a9b7c6d02c95bbb92c26081a405498615
Lucas <Lucasdeapool@gamil.com>
2025-04-30 20:11:33 -0300

    game calls method

--- src/TicTacToe/Application.java
+++ src/TicTacToe/Application.java
@@ -1,23 +1,10 @@
 package TicTacToe;
-
-import TicTacToe.Board.Board;
-
-
-import static TicTacToe.Symbols.Symbols.O;
-import static TicTacToe.Symbols.Symbols.X;
+import TicTacToe.Game.Game;
 
 public class Application {
 
     public static void main(String[] args){
 
-        // Funcionando
-
-        Board p = new Board();
-        p.placeSymbol(0,0,X);
-        p.placeSymbol(1,0,X);
-        p.placeSymbol(2,0,X);
-        p.printBoard();
-
-        System.out.println(p.hasWinner());
+        new Game().start();
     }
 }
