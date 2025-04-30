01a08a204b8c53c2303560f9e8b02fc1fa879322
Lucas <Lucasdeapool@gamil.com>
2025-04-30 20:13:51 -0300

    created the junction of all classes

--- src/TicTacToe/Game/Game.java
+++ src/TicTacToe/Game/Game.java
@@ -10,16 +10,58 @@ import java.util.*;
 public class Game {
 
     private Players player;
+    private Players player2;
     private SaveGame saveGame;
     private Board board;
-    private Symbols symbols;
+
 
     Scanner sc = new Scanner(System.in);
-    public void start(){
+    private  boolean gameOver = false;
 
-        boolean gameOver = false;
-        while(!gameOver){
-            new Board().printBoard();
+    public void start() {
+
+        System.out.println("First player name");
+        String currentPlayer1 = sc.nextLine();
+        player = new Players(currentPlayer1, Symbols.X);
+
+        System.out.println("Second player name");
+        String currentPlayer2 = sc.nextLine();
+        player2 = new Players(currentPlayer2, Symbols.O);
+
+        board = new Board();
+        Players currentPlayer = player;
+        System.out.println("Game started");
+
+        do {
+            System.out.println(currentPlayer.getNome() + "'s turn");
+            board.printBoard();
+            currentPlayer.play(board);
+            System.out.println();
+
+            if (board.hasWinner(currentPlayer.getSymbol())) {
+                System.out.println(currentPlayer.getNome() + " wins!");
+                board.printBoard();
+                gameOver = true;
+            } else if (board.checkDraw()) {
+                System.out.println("It's a draw!");
+                board.printBoard();
+                gameOver = true;
+            } else {
+                currentPlayer = (currentPlayer == player) ? player2 : player;
+            }
+
+        } while(!gameOver);
+
+        System.out.println("Do you want to save the game?");
+        System.out.println("[1] - Yes");
+        System.out.println("[0] - No");
+        int option = sc.nextInt();
+        if(option == 1) {
+            saveGame.save();
+        } else {
+            System.exit(0);
         }
+
+
     }
 }
